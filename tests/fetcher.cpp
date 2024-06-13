8a851a9832892b6065d456b5b7cef05f2ae648f7
Roiqk <112021405+Roiqk7@users.noreply.github.com>
2024-06-13 14:49:21 +0200

    Create fetcher.cpp

---
+++ tests/fetcher.cpp
@@ -0,0 +1,37 @@
+/*
+Date: 12/06/2024
+
+Description:
+
+Notes: x
+*/
+
+#include <gtest/gtest.h>
+#include <spdlog/spdlog.h>
+#include "../src/fetcher.cpp"
+#include "../src/tools.cpp"
+
+namespace Fetcher
+{
+        // Test for SPY ETF data fetch
+        TEST(FetcherTest, FetchData)
+        {
+                // URL for fetching SPY ETF data
+                std::string url = Fetcher::Constants::API_URL + Constants::ETF_URL_ENDPOINT + Constants::SPY + Constants::API_KEY_PARAM + Constants::API_KEY;
+                std::string readBuffer;
+
+                // Hide the API key in the log
+                std::string hiddenApiKey = Tools::hide_api_key(url);
+                spdlog::info("URL: {}", hiddenApiKey);
+
+                // Initialize libcurl and set the necessary options
+                CURL* curl = init_curl(url, readBuffer);
+                ASSERT_TRUE(curl != nullptr);
+
+                // Perform the HTTP request
+                CURLcode res = perform_request(curl);
+                ASSERT_EQ(res, CURLE_OK);
+
+                spdlog::info("Received data: {}", readBuffer);
+        }
+}
