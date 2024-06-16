d3df25801c8853a03fa98a895a8f068d011bcafc
Roiqk7 <petrsevecek@icloud.com>
2024-06-16 18:47:29 +0200

    Added inputHandler.cpp test for missing argument.

--- test/inputHandler.cpp
+++ test/inputHandler.cpp
@@ -13,7 +13,7 @@ Notes: x
 
 namespace Fetcher
 {
-        // Test for catching invalid user input in the userInputValidator() function
+        // Test for catching invalid user input in the userInputValidator() function - empty argument
         TEST(InputHandlerTest, userInputValidatorTest)
         {
                 // Construct the file path dynamically
@@ -24,6 +24,25 @@ namespace Fetcher
 
                 // Invalid user input - empty time frame argument (invalid eg. in this case: Fetcher::fetchHistoricalData("AAPL", "2021-01-01", "2021-01-05", "", "fmp", "API_KEY");)
                 std::vector<std::string> args = {"AAPL", "2021-01-01", "2021-01-05", "", "fmp", "vKoDVGFjjGHf5uWq3O0ztQ0w6CfMfxIa"};
+
+                // Argument names
+                std::vector<std::string> argNames = {"tickerSymbol", "fromDate", "toDate", "timeFrame", "api", "apiKey"};
+
+                // Catch the invalid user input exception
+                ASSERT_THROW(InputHandler::userInputValidator(args, argNames), std::invalid_argument);
+        }
+
+        // Test for catching invalid user input in the userInputValidator() function - missing argument
+        TEST(InputHandlerTest, userInputValidatorTest2)
+        {
+                // Construct the file path dynamically
+                std::string testName = ::testing::UnitTest::GetInstance()->current_test_info()->name();
+
+                // Log
+                spdlog::info("{} test started. Catching invalid user input...", testName);
+
+                // Invalid user input - missing time frame argument (invalid eg. in this case: Fetcher::fetchHistoricalData("AAPL", "2021-01-01", "2021-01-05", "fmp", "vKoDVGFjjGHf5uWq3O0ztQ0w6CfMfxIa");)
+                std::vector<std::string> args = {"AAPL", "2021-01-01", "2021-01-05", "fmp", "vKoDVGFjjGHf5uWq3O0ztQ0w6CfMfxIa"};
 
                 // Argument names
                 std::vector<std::string> argNames = {"tickerSymbol", "fromDate", "toDate", "timeFrame", "api", "apiKey"};
