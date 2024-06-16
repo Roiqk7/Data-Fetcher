f2df5d0f5d8fa03f6d4faad3aaea8c9a733b4a72
Roiqk7 <petrsevecek@icloud.com>
2024-06-16 21:41:54 +0200

    Added to store definitions and declarations of custom exceptions used in the code.

---
+++ src/exception.cpp
@@ -0,0 +1,11 @@
+/*
+Date: 16/06/2024
+
+Description: This file contains definitions of custom exceptions used in the programme.
+
+Notes: x
+*/
+
+#include <exception>
+#include <string>
+#include "../src/include/exception.h"
---
+++ src/include/exception.h
@@ -0,0 +1,12 @@
+/*
+Date: 16/06/2024
+
+Description: This file contains declarations of custom exceptions used in the programme.
+
+Notes: x
+*/
+
+#ifndef EXCEPTION_H
+#define EXCEPTION_H
+
+#endif // !EXCEPTION_H
