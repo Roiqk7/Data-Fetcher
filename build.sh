0339c571c616217ca8d3db23b9604b3c7043f378
Roiqk7 <petrsevecek@icloud.com>
2024-06-20 14:04:29 +0200

    Improved functionality and added a few echos.

--- build.sh
+++ build.sh
@@ -3,15 +3,30 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Create a build directory and enter it
-mkdir -p build
-cd build
+# Create a build directory if it doesn't exist
+if [ ! -d "build" ]; then
+        echo "Creating build directory..."
+        mkdir build
+fi
+
+# Check if the current directory is not already 'build'
+if [[ $(basename "$PWD") != "build" ]]; then
+        echo "Moving to the build directory..."
+        cd build
+fi
+
+echo "Configuring the project with CMake..."
 
 # Configure the project with CMake
 cmake ..
 
+echo "Building the project..."
+
 # Build the project
 make
 
-# Go back to the original directory
-cd -
+# Go back to the original directory only if we changed directories
+if [[ $(basename "$PWD") == "build" ]]; then
+        echo "Moving back to the original directory..."
+        cd -
+fi
