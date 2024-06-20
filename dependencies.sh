19fec16c6f1eb168858ae2e8eba3e32e7624f994
Roiqk7 <petrsevecek@icloud.com>
2024-06-20 13:29:33 +0200

    Added functionality. Now if a user doesnt have homebrew it will automatically be installed.

--- dependencies.sh
+++ dependencies.sh
@@ -3,5 +3,19 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Function to check if Homebrew is installed and install it if it's not
+install_homebrew()
+{
+        if ! command -v brew >/dev/null 2>&1; then
+                echo "Homebrew not found. Installing Homebrew..."
+                /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        else
+                echo "Homebrew is already installed."
+        fi
+}
+
+# Install Homebrew if it's not installed
+install_homebrew
+
 # Install dependencies
 brew install boost cmake curl googletest jsoncpp spdlog
