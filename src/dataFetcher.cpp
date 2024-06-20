7257c385dea67da1bb1287d0369e746923422ff5
Roiqk7 <petrsevecek@icloud.com>
2024-06-20 16:21:43 +0200

    Cosmetics

--- src/dataFetcher.cpp
+++ src/dataFetcher.cpp
@@ -27,7 +27,8 @@ namespace Fetcher
 
         @return: The fetched historical data.
         */
-        Json::Value fetchHistoricalData(const std::string& tickerSymbol, const std::string& fromDate, const std::string& toDate, const std::string& timeFrame, const std::string& api, const std::string& apiKey)
+        Json::Value fetchHistoricalData(const std::string& tickerSymbol, const std::string& fromDate,
+                const std::string& toDate, const std::string& timeFrame, const std::string& api, const std::string& apiKey)
         {
                 // Validate user input
                 InputHandler::userInputValidator({tickerSymbol, fromDate, toDate, timeFrame, api, apiKey},
